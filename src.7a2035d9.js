ddcff64fd50732b2da274a56ccfba4bf965b9a57
Kyle-Roman <77810625+Kyle-Roman@users.noreply.github.com>
2021-08-05 18:35:05 +0000

    Deploying to gh-pages from @ Kyle-Roman/goit-js-hw-11-promisification@93d22f4315e484192b89006317a29aab27f0c79a 🚀

--- src.7a2035d9.js
+++ src.7a2035d9.js
@@ -1,8 +1,8 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"dYe9":[function(require,module,exports) {
-const o=o=>Promise((e,l)=>{setTimeout(()=>{e(console.log("hello"))},o)}),e=o=>console.log(`Resolved after ${o}ms`);o(1e3);
+const e=e=>new Promise(o=>{setTimeout(()=>{o(e)},e)}),o=e=>console.log(`Resolved after ${e}ms`);e(2e3).then(o),e(1e3).then(o),e(1500).then(o);
 },{}],"nlcn":[function(require,module,exports) {
 
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./js/delay"),require("./js/toggle_user"),require("./js/transactions");
 },{"./js/delay":"dYe9","./js/toggle_user":"nlcn","./js/transactions":"nlcn"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-project-template/src.12816011.js.map
+//# sourceMappingURL=/parcel-project-template/src.7a2035d9.js.map
