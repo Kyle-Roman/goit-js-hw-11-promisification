93d22f4315e484192b89006317a29aab27f0c79a
Kirill Romanenko <brs.group.com@gmail.com>
2021-08-05 21:34:19 +0300

    finished task 1

--- src/js/delay.js
+++ src/js/delay.js
@@ -1,17 +1,15 @@
-const delay = ms => {
-    return Promise((good, evil) => {
+const delay = (ms) => {
+    return new Promise(resolve => {
         setTimeout(() => {
-            good(console.log('hello'));
+            resolve(ms);
         }, ms);
     })
 };
-
-const logger = time => console.log(`Resolved after ${time}ms`);
+const logger = ms => console.log(`Resolved after ${ms}ms`);
 
 // Вызовы функции для проверки
-// delay(2000).then(logger); // Resolved after 2000ms
-// delay(1000).then(logger); // Resolved after 1000ms
-// delay(1500).then(logger); // Resolved after 1500ms
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
 
 
-delay(1000);
